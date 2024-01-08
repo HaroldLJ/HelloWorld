5160fee3b8a77c9bcaa9d4623e4627fb5bcbf30a
HaroldLJ <155929160+HaroldLJ@users.noreply.github.com>
2024-01-07 20:44:52 -0500

    Add files via upload

---
+++ Loop Chapter 7.py
@@ -0,0 +1,21 @@
+"""
+count = 1
+while count < 5:
+    print(count,' ',end=' ')
+    count +=1
+print()
+
+a = 2
+b = 2
+while b <=10:
+    print(a, 'x',  b, '=', a * b)
+    b +=2  #Iterating value of b to not make it go forever
+
+for c in range(2, 11):  #variable does not matter, does not need to be defined
+    if c % 2 == 0:  
+        print(a, 'x',  c, '=', a * c)
+        #No iteration in for loop like while loop
+"""
+
+while 0 != 1:
+    print(0)
